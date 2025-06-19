f6afb8954687f762422f77c9dbe15b6916778052
Pratham-Malhotraa <167972362+Pratham-Malhotraa@users.noreply.github.com>
2025-06-20 01:06:49 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-cd path/to/premium-api
-git init
-git add .
-git commit -m "Initial Render deployment"
-git remote add origin https://github.com/your-username/premium-api.git
-git push -u origin master
-#!/bin/bash
-Rscript app.R
